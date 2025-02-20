180a0c249bcfce3c658915fc6960e1ff14413f60
Parallax10 <131243633+Parallax10@users.noreply.github.com>
2025-02-20 21:40:17 +0100

    upd:comienzo ej2 repaso poo

---
+++ REPASOPOO/ej2/src/Estudiante.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Estudiante extends Persona{
+    String curso;
+    //ArrayList<Asistencia> Asistencia;
+    @Override
+    public void registrarAsistencia(String clase, String fecha) {
+
+    }
+}
---
+++ REPASOPOO/ej2/src/Persona.java
@@ -0,0 +1,4 @@
+public abstract class Persona {
+    String nombre,apellido;
+    public abstract void registrarAsistencia(String clase, String fecha);
+}
